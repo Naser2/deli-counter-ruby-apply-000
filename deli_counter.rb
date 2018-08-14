779f26e9b6a4848195ffa2a5cfffe1c36da4d6b0
Nasser Sanou <nassersanou23@gmail.com>
2018-08-14 14:57:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,21 +16,36 @@ end
 line(katz_deli)
 
 
-def take_a_number(katz_deli, name)
+def take_a_number(katz_deli)
+
+  katz_deli =  [1,2,3,4,5]
+  katz_deli[] += 1
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 
 
-$$current_number = 0
-def now_serving(katz_deli)
-	 $$current_number += 1
-	 $$current_number
-	 # if katz_deli.empty? == true
-	 #   puts "There is nobody waiting to be served!"
-	 # else
-	 #   name = katz_deli.shift
-	 #   puts "Currently serving #{name}."
-	 # end
-	end
+# def now_serving(katz_deli)
+	
+# 	  if katz_deli.empty? == true
+# 	    puts "There is nobody waiting to be served!"
+# 	  else
+# 	    name = katz_deli.shift
+# 	    puts "Currently serving #{name}."
+# 	    "Currently serving Katie"
+# 	    ["katie", "Natalie"]
+# 	  end
+	  
+# 	  $$current_number = 0
+# def now_serving(katz_deli)
+# 	 $$current_number += 1
+# 	 $$current_number
+# 	 # if katz_deli.empty? == true
+# 	 #   puts "There is nobody waiting to be served!"
+# 	 # else
+# 	 #   name = katz_deli.shift
+# 	 #   puts "Currently serving #{name}."
+# 	 # end
+# 	end
+# 	end
